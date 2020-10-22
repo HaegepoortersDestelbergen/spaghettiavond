39e279a38c91e5828c5b1e6bb142cca97da71564
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 17:34:59 +0200

    Update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 // const fetch = require('node-fetch');
 // const csv = require('csvtojson')
 
+import * as fetch from 'node-fetch';
 // import * as csv from 'csvtojson';
 
 export default (req, res) => {
-    res.send('data');
+    // res.send('data');
     
-    fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
+    fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`, {
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(body => body.json())
+    .then((data) => res.send(data))
     
     // fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
     // .then(resp => resp.json())
