5e6c855a928c6c811eee9ccc4df5787447ca8a43
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 16:24:13 +0200

    Update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,42 @@
-export default (req, res) => {
-    res.send({
-        test: 'test working'
-    })
+const GSheetReader = require('g-sheets-api');
+const request=require('request')
+const fetch = require('node-fetch');
+const csv = require('csvtojson')
+
+const { SHEET_URI } = process.env
+
+module.exports = async (req, res) => {
+    const {email} = req.query
+    
+    const getResponses = () => {
+        fetch(SHEET_URI)
+        .then(res => res.text())
+        .then(async (data) => {
+            csv({
+                noheader:true,
+                output: "csv"
+            })
+            .fromString(data)
+            .then((csvRow)=>{ 
+                const l = csvRow.length;
+                const lel = csvRow.slice(3, l);
+                res.send(lel);
+            })
+        })
+        .catch(err => {console.log(err)})
+    }
+    
+    
+    
+    
+    // GSheetReader(options, results => {
+    //     console.log('hello');
+    //     console.log(results);
+    // }).catch(err => {
+    //     console.log(err);
+    // })
+    
+    // res.send({
+    //     test: 'test working'
+    // })
 }
