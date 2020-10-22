69b27adeb4110b000287a09d2913a2f97add1aa3
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 17:21:50 +0200

    Update

--- api/index.js
+++ api/index.js
@@ -1,25 +1,28 @@
-const fetch = require('node-fetch');
-const csv = require('csvtojson')
+// const fetch = require('node-fetch');
+// const csv = require('csvtojson')
 
-module.exports = async (req, res) => {
+import * as fetch from 'node-fetch';
+import * as csv from 'csvtojson';
+
+export default async (req, res) => {
+    
+    // res.send('data');
     // const {email} = req.query
     
     fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
     .then(resp => resp.json())
-    .then(async (data) => {
-        res.send(data);
-        // csv({
-        //     noheader: true,
-        //     output: "csv"
-        // })
-        // .fromString(data)
-        // .then((csvRow)=>{ 
-        //     console.log(csvRow);
-        //     // const l = csvRow.length;
-        //     // const lel = csvRow.slice(3, l);
-        //     // res.send(lel);
-        //     res.send(csvRow);
-        // })
+    .then(async ({contents}) => {
+        csv.default({
+            noheader: true,
+            output: "csv"
+        })
+        .fromString(contents)
+        .then((csvRow)=>{ 
+            // const l = csvRow.length;
+            // const lel = csvRow.slice(3, l);
+            // res.send(lel);
+            res.send(csvRow);
+        })
     })
     .catch(err => {res.send(err)})
 }
