38e21808da433905d8527e8f4b6627d7c69df78a
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 17:11:46 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,22 +5,21 @@ module.exports = async (req, res) => {
     // const {email} = req.query
     
     fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
-    .then(res => res.json())
-    .then(res => res.json())
-    // .then(async (data) => {
-    //     res.send(data);
-    //     // csv({
-    //     //     noheader: true,
-    //     //     output: "csv"
-    //     // })
-    //     // .fromString(data)
-    //     // .then((csvRow)=>{ 
-    //     //     console.log(csvRow);
-    //     //     // const l = csvRow.length;
-    //     //     // const lel = csvRow.slice(3, l);
-    //     //     // res.send(lel);
-    //     //     res.send(csvRow);
-    //     // })
-    // })
+    .then(resp => resp.json())
+    .then(async (data) => {
+        resp.send(data);
+        // csv({
+        //     noheader: true,
+        //     output: "csv"
+        // })
+        // .fromString(data)
+        // .then((csvRow)=>{ 
+        //     console.log(csvRow);
+        //     // const l = csvRow.length;
+        //     // const lel = csvRow.slice(3, l);
+        //     // res.send(lel);
+        //     res.send(csvRow);
+        // })
+    })
     .catch(err => {res.send(err)})
 }
