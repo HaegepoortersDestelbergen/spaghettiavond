44cfa54e9e113dd7ba7446e25128a7f2c6cebd2a
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 22:14:42 +0200

    Update

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ export default async ({query: {email}}, res) => {
                 }
             },
             comment: comment,
-            paystatus: payed
+            paystatus: payed == 'FALSE' ? false : true
         }
     }
     
