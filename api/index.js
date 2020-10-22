1183e8d6894a26919da001dba518244ec7ac28b1
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 17:37:52 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import * as fetch from 'node-fetch';
 export default (req, res) => {
     // res.send('data');
     
-    fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`, {
+    fetch(`https://api.allorigins.win/get?url=https://raw.githubusercontent.com/lennertderyck/photo-sorter/master/package.json`, {
         mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json'
