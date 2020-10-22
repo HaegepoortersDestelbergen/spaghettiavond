fb37a7cfab090922b6b6afc24fbc121c8cd1c4f4
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 16:59:40 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,35 +6,22 @@ const csv = require('csvtojson')
 const { SHEET_URI } = process.env
 
 module.exports = async (req, res) => {
-    const {email} = req.query
+    // const {email} = req.query
     
-        fetch(SHEET_URI)
-        .then(res => res.text())
-        .then(async (data) => {
-            csv({
-                noheader:true,
-                output: "csv"
-            })
-            .fromString(data)
-            .then((csvRow)=>{ 
-                const l = csvRow.length;
-                const lel = csvRow.slice(3, l);
-                res.send(lel);
-            })
+    fetch(SHEET_URI)
+    .then(res => res.text())
+    .then(async (data) => {
+        csv({
+            noheader:true,
+            output: "csv"
         })
-        .catch(err => {console.log(err)})
-    
-    
-    
-    
-    // GSheetReader(options, results => {
-    //     console.log('hello');
-    //     console.log(results);
-    // }).catch(err => {
-    //     console.log(err);
-    // })
-    
-    // res.send({
-    //     test: 'test working'
-    // })
+        .fromString(data)
+        .then((csvRow)=>{ 
+            // const l = csvRow.length;
+            // const lel = csvRow.slice(3, l);
+            // res.send(lel);
+            res.send(csvRow);
+        })
+    })
+    .catch(err => {console.log(err)})
 }
