9376f4be5b6e05f1c5cbc3b08710087796c1419b
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 18:08:25 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,41 @@
 import * as csv from 'csvtojson';
 import fetch from 'node-fetch';
 
-export default async (req, res) => {
-    console.log(req)
+export default async ({query: {email}}, res) => {    
+    const getSheetData = () => {
+        return new Promise((resolve, reject) => {
+            fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
+            .then(response => response.text())
+            .then(async (data) => {
+                csv.default({
+                    noheader: true,
+                    output: "csv"
+                })
+                .fromString(data)
+                .then((csvRow)=>{ 
+                    const l = csvRow.length;
+                    const lel = csvRow.slice(3, l);
+                    resolve(lel)
+                })
+            })
+            .catch(err => {
+                reject(err);
+            })
+        })
+    }
     
-    fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
-    .then(resp => resp.text())
-    .then(async (data) => {
-        console.log(data);
-        csv.default({
-            noheader: true,
-            output: "csv"
+    const getRecordByEmail = async (email) => {
+        const data = await getSheetData();
+        return await data.filter((r) => r[1] == email);
+    }
+    
+    if (email) {
+        const data = await getRecordByEmail(email);
+        res.send(data);
+    } else {
+        res.send({
+            error: 'email was undefined'
         })
-        .fromString(data)
-        .then((csvRow)=>{ 
-            const l = csvRow.length;
-            const lel = csvRow.slice(3, l);
-            res.send(lel);
-        })
-    })
-    .catch(err => {res.send(err)})
+    }
+    
 }
