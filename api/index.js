a0ffb4397497bd70958a3782a804f922c661ef8d
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-22 17:50:46 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,41 +3,41 @@ import fetch from 'node-fetch';
 
 export default async (req, res) => {
     console.log(req)
-    const prom = () => {
-        return new Promise((resolve, reject) => {
-            fetch(`https://api.allorigins.win/get?url=https://raw.githubusercontent.com/lennertderyck/photo-sorter/master/package.json`, {
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            })
-            .then(body => {return body.json()})
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((err) => {
-                reject(err)
-            })
+    // const prom = () => {
+    //     return new Promise((resolve, reject) => {
+    //         fetch(`https://api.allorigins.win/get?url=https://raw.githubusercontent.com/lennertderyck/photo-sorter/master/package.json`, {
+    //             mode: 'no-cors',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //         })
+    //         .then(body => {return body.json()})
+    //         .then((data) => {
+    //             resolve(data);
+    //         })
+    //         .catch((err) => {
+    //             reject(err)
+    //         })
+    //     })
+    // }
+    
+    // const data = await prom();
+    // res.send(await data);
+    
+    fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
+    .then(resp => resp.json())
+    .then(async ({contents}) => {
+        csv.default({
+            noheader: true,
+            output: "csv"
         })
-    }
-    
-    const data = await prom();
-    res.send(await data);
-    
-    // fetch(`https://api.allorigins.win/get?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMUFYniXHfMzwlr7Ir6U113p1XgXpQt4F1YP_Y0fUUANBx74Aga1rkMjBM9GlD47vx3GHWr6JRI-y/pub?output=csv`)
-    // .then(resp => resp.json())
-    // .then(async ({contents}) => {
-    //     csv.default({
-    //         noheader: true,
-    //         output: "csv"
-    //     })
-    //     .fromString(contents)
-    //     .then((csvRow)=>{ 
-    //         // const l = csvRow.length;
-    //         // const lel = csvRow.slice(3, l);
-    //         // res.send(lel);
-    //         res.send(csvRow);
-    //     })
-    // })
-    // .catch(err => {res.send(err)})
+        .fromString(contents)
+        .then((csvRow)=>{ 
+            // const l = csvRow.length;
+            // const lel = csvRow.slice(3, l);
+            // res.send(lel);
+            res.send(csvRow);
+        })
+    })
+    .catch(err => {res.send(err)})
 }
